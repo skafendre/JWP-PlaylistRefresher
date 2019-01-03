77bdfecfc43b118a377f19b23ba00400e1ed033a
Eliott Lambert <eliott.lambert@gmail.com>
2019-01-03 13:59:58 +0100

    Break credentials variables into their own secure file.

--- main.php
+++ main.php
@@ -3,17 +3,15 @@ header('Content-type: text/plain; charset=utf-8');
 
 require_once('api.php');
 require_once('class/AlgoPlaylistKonbini.class.php');
+require_once ('credentials.php');
 
 // error handling
-function customError($error_level, $error_message,
- $error_file, $error_line, $error_context) {
+function customError($error_level, $error_message) {
     echo "<b>Error:</b> [$error_level] $error_message";
 }
 set_error_handler("customError", E_USER_NOTICE);
 
 // credentials JWPlatform
-$secret = '3RkF3cAHfiK1mB3b4jHMAc5h';
-$key = 'hFhUg05J';
 
 $playlistTag = 'playlist Konbini';
 $channelKey = 'GOHRhAkf';
